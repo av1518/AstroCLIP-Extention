e68aa06812e44bd17bdc939114d37ae2b4d4ea08
av1518 <65544264+av1518@users.noreply.github.com>
2024-05-09 13:35:23 +0100

    chore: Update dataloader batch size and number of workers

--- src/train.py
+++ src/train.py
@@ -51,58 +51,67 @@ wandb.login()
 
 sns.set_style("ticks")
 sns.set_context("paper", font_scale=1.5, rc={"lines.linewidth": 2})
+
+
 # %% Get datasets
-# def main():
+def main():
+    torch.set_float32_matmul_precision("medium")
 
-CACHE_DIR = "C:\datasets_astroclip"
-dataset = load_dataset("src/datasets_files/legacy_survey.py", cache_dir=CACHE_DIR)
-dataset.set_format(type="torch", columns=["image", "spectrum"])
-# %%
-# Create the dataloaders
-train_dataloader = DataLoader(
-    dataset["train"], batch_size=10, shuffle=True, num_workers=0
-)
-val_dataloader = DataLoader(
-    dataset["test"], batch_size=10, shuffle=False, num_workers=0
-)
+    CACHE_DIR = "C:\datasets_astroclip"
+    dataset = load_dataset("src/datasets_files/legacy_survey.py", cache_dir=CACHE_DIR)
+    dataset.set_format(type="torch", columns=["image", "spectrum"])
+    #
+    # Create the dataloaders
+    print("Creating dataloaders")
+    train_dataloader = DataLoader(
+        dataset["train"], batch_size=512, shuffle=True, num_workers=3
+    )
+    val_dataloader = DataLoader(
+        dataset["test"], batch_size=512, shuffle=False, num_workers=3
+    )
 
-# %% Load image and spectrum models
-checkpoint_path = "data/weights/resnet50.ckpt"
-moco_model = Moco_v2.load_from_checkpoint(checkpoint_path=checkpoint_path)
-# extract the backbone model
-backbone = moco_model.encoder_q
-im_encoder = OutputExtractor(backbone)
-sp_encoder = ExtendedSpender()
+    print("Dataloaders created")
 
-# %%
-# Setting up image augmentations
-image_transforms = Compose(
-    [
-        RandomRotation(45, interpolation=InterpolationMode.BILINEAR),
-        RandomHorizontalFlip(),
-        RandomVerticalFlip(),
-        CenterCrop(96),
-    ]
-)
+    #  Load image and spectrum models
+    checkpoint_path = "data/weights/resnet50.ckpt"
+    moco_model = Moco_v2.load_from_checkpoint(checkpoint_path=checkpoint_path)
+    # extract the backbone model
+    backbone = moco_model.encoder_q
+    im_encoder = OutputExtractor(backbone)
+    sp_encoder = ExtendedSpender()
 
-wandb_logger = WandbLogger(log_model="all", project="astroclip", name="clip")
+    #
+    # Setting up image augmentations
+    image_transforms = Compose(
+        [
+            RandomRotation(45, interpolation=InterpolationMode.BILINEAR),
+            RandomHorizontalFlip(),
+            RandomVerticalFlip(),
+            CenterCrop(96),
+        ]
+    )
 
-model = AstroCLIP(im_encoder, sp_encoder, image_transforms)
+    wandb_logger = WandbLogger(
+        log_model="all", project="astroclip", name="clip-512-batch-1"
+    )
 
-trainer = L.Trainer(
-    max_epochs=10,
-    callbacks=[
-        ModelCheckpoint(
-            every_n_epochs=1,
-        )
-    ],
-    logger=wandb_logger,
-)
+    model = AstroCLIP(im_encoder, sp_encoder, image_transforms)
 
-trainer.fit(model, train_dataloader, val_dataloader)
+    trainer = L.Trainer(
+        max_epochs=10,
+        callbacks=[
+            ModelCheckpoint(
+                every_n_epochs=1,
+            )
+        ],
+        logger=wandb_logger,
+        log_every_n_steps=1,
+    )
+
+    trainer.fit(model, train_dataloader, val_dataloader)
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
 # %%
